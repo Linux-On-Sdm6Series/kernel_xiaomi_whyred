e109a5798c594c12c7e9ff0b713497774e7e7d04
dopaemon <polarisdp@gmail.com>
2020-05-31 00:53:29 +0700

    Revert "UBUNTU: SAUCE: fs: Treat foreign mounts as nosuid"
    This reverts commit 334c97b057d14e6032882f605208353999847004.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2159,7 +2159,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *new_tsec)
 {
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
-	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
+	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
 	int rc;
 
 	if (!nnp && !nosuid)
