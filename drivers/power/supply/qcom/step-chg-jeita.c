7efd6cd626833723b269a9b3771b77726c5a273f
psndna88 <psndna88@gmail.com>
2020-06-04 23:26:06 +0530

    power: supply: set max charging temperature limits v4
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -123,10 +123,12 @@ static struct jeita_fcc_cfg jeita_fcc_config = {
 	.hysteresis	= 0, /* 1degC hysteresis */
 	.fcc_cfg	= {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
-		{0,		50,		300000},
-		{51,		150,		900000},
-		{151,	450,		2900000},
-		{451,	600,		1500000},
+		{0,		50,		 400000},
+		{51,		150,		1200000},
+		{151,		430,		2500000},
+		{431,		450,		2000000},
+		{451,		470,		1500000},
+		{471,		600,		 800000},
 	},
 };
 #elif defined(CONFIG_KERNEL_CUSTOM_F7A)
@@ -138,8 +140,10 @@ static struct jeita_fcc_cfg jeita_fcc_config = {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
 		{0,		50,		400000},
 		{51,		150,		1200000},
-		{151,	450,		2900000},
-		{451,	600,		2000000},
+		{151,		430,		2500000},
+		{431,		450,		2000000},
+		{451,		470,		1500000},
+		{471,		600,		 800000},
 	},
 };
 #elif defined(CONFIG_KERNEL_CUSTOM_E7S)
